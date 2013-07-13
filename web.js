67fa9147841598d0ede9b2ccac60d8921f1e843c
Fabiano V Siegel <biaosiegel@gmail.com>
2013-07-13 02:14:08 +0000

    reading index.html file and send content as response

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require ('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var content = fs.readFileSync("index.html");
+  var contentAsStr= content.toString(); 
+  response.send(contentAsStr);
 });
 
 var port = process.env.PORT || 5000;
